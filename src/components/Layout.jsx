cf4b8bf182ee59bce381f6332161c735ee6732a4
David D Lawson <hello@ddlawson.com>
2023-05-07 20:08:30 +0100

    Change doc references to guide & url path to learn

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -16,7 +16,12 @@ const navigation = [
     title: 'Introduction',
     links: [
       { title: 'Getting started', href: '/' },
-      { title: 'Installation', href: '/docs/installation' },
+      { title: 'Motivation', href: '/learn/motivation' },
+      {
+        title: 'Additional Resources',
+        href: '/learn/motivation',
+        active: false,
+      },
     ],
   },
   {
@@ -25,49 +30,49 @@ const navigation = [
       {
         title: 'Understanding caching',
         active: false,
-        href: '/docs/understanding-caching',
+        href: '/learn/understanding-caching',
       },
       {
         title: 'Predicting user behavior',
-        href: '/docs/predicting-user-behavior',
+        href: '/learn/predicting-user-behavior',
       },
-      { title: 'Basics of time-travel', href: '/docs/basics-of-time-travel' },
+      { title: 'Basics of time-travel', href: '/learn/basics-of-time-travel' },
       {
         title: 'Introduction to string theory',
-        href: '/docs/introduction-to-string-theory',
+        href: '/learn/introduction-to-string-theory',
       },
-      { title: 'The butterfly effect', href: '/docs/the-butterfly-effect' },
+      { title: 'The butterfly effect', href: '/learn/the-butterfly-effect' },
     ],
   },
   {
-    title: 'Advanced guides',
+    title: 'Advanced learns',
     links: [
-      { title: 'Writing plugins', href: '/docs/writing-plugins' },
-      { title: 'Neuralink integration', href: '/docs/neuralink-integration' },
-      { title: 'Temporal paradoxes', href: '/docs/temporal-paradoxes' },
-      { title: 'Testing', href: '/docs/testing' },
-      { title: 'Compile-time caching', href: '/docs/compile-time-caching' },
+      { title: 'Writing plugins', href: '/learn/writing-plugins' },
+      { title: 'Neuralink integration', href: '/learn/neuralink-integration' },
+      { title: 'Temporal paradoxes', href: '/learn/temporal-paradoxes' },
+      { title: 'Testing', href: '/learn/testing' },
+      { title: 'Compile-time caching', href: '/learn/compile-time-caching' },
       {
         title: 'Predictive data generation',
-        href: '/docs/predictive-data-generation',
+        href: '/learn/predictive-data-generation',
       },
     ],
   },
   {
     title: 'API reference',
     links: [
-      { title: 'CacheAdvance.predict()', href: '/docs/cacheadvance-predict' },
-      { title: 'CacheAdvance.flush()', href: '/docs/cacheadvance-flush' },
-      { title: 'CacheAdvance.revert()', href: '/docs/cacheadvance-revert' },
-      { title: 'CacheAdvance.regret()', href: '/docs/cacheadvance-regret' },
+      { title: 'CacheAdvance.predict()', href: '/learn/cacheadvance-predict' },
+      { title: 'CacheAdvance.flush()', href: '/learn/cacheadvance-flush' },
+      { title: 'CacheAdvance.revert()', href: '/learn/cacheadvance-revert' },
+      { title: 'CacheAdvance.regret()', href: '/learn/cacheadvance-regret' },
     ],
   },
   {
     title: 'Contributing',
     links: [
-      { title: 'How to contribute', href: '/docs/how-to-contribute' },
-      { title: 'Architecture guide', href: '/docs/architecture-guide' },
-      { title: 'Design principles', href: '/docs/design-principles' },
+      { title: 'How to contribute', href: '/learn/how-to-contribute' },
+      { title: 'Architecture learn', href: '/learn/architecture-learn' },
+      { title: 'Design principles', href: '/learn/design-principles' },
     ],
   },
 ]

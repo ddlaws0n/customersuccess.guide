1c51c6c367393a3fd05a9c42e69606bbcdba8a2b
David D Lawson <hello@ddlawson.com>
2023-05-08 09:07:56 +0100

    Add next-sitemap for seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,25 @@
+const SITE_URL = process.env.SITE_URL || 'https://customersuccess.guide'
+
+const NEXT_SSG_FILES = [
+  '/*.json$',
+  '/*_buildManifest.js$',
+  '/*_middlewareManifest.js$',
+  '/*_ssgManifest.js$',
+  '/*.js$',
+]
+
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  SITE_URL,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: NEXT_SSG_FILES,
+      },
+    ],
+  },
+}
+
+module.exports = config
